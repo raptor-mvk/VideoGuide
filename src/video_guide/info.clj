56dea069957f17d1cbfaab3e6af4f027dda3d023
raptor-mvk <m.v.kamorin@gmail.com>
2015-06-24 14:56:53 +0300

    Added default-entity and table into EntityInfo
    Added default-entity and table into EntityInfo. Changed sql-fields
    implementation in CommonEntityInfo for returning proper sql-field
    description instead of FieldInfo list..

--- src/video_guide/info.clj
+++ src/video_guide/info.clj
@@ -2,25 +2,67 @@
   ^{:author raptor_MVK}
   video_guide.info)
 
+(declare check-fields-info default-field-value)
+
 (defprotocol EntityInfo
   (sql-fields [_])
   (form-fields [_])
-  (columns [_]))
+  (columns [_])
+  (table [_])
+  (default-entity [this]))
 
 (defrecord CommonEntityInfo [name fields-info]
   EntityInfo
   (sql-fields [_]
-    (filter #(.sql? %) fields-info))
+    (map #(.sql-field %) (filter #(.sql? %) fields-info)))
   (form-fields [_]
     (sort (fn [x y] (< (.field-order x) (.field-order y)))
       (filter #(.field? %) fields-info)))
   (columns [_]
     (sort (fn [x y] (< (.column-order x) (.column-order y)))
-      (filter #(.column? %) fields-info))))
+      (filter #(.column? %) fields-info)))
+  (table [_] name)
+  (default-entity [this]
+    (let [field-values (map default-field-value (form-fields this))]
+      (if (not-empty field-values)
+        (reduce into field-values)
+        nil))))
 
 (defprotocol FieldInfo
+  (field [_])
+  (label [_])
+  (getter [_])
+  (default [_])
+  (column [_])
+  (sql-field [_])
+  (sql-name [_])
   (field-order [_])
   (column-order [_])
   (field? [_])
   (column? [_])
   (sql? [_]))
+
+(defn make-entity-info
+  "Given name and fields-info list, returns CommonEntityInfo. If fields-info
+  is empty or check-fields-info(fields-info) returns false, then throws
+  IllegalArgumentException"
+  [name fields-info]
+  (if (and (not-empty fields-info) (check-fields-info fields-info))
+    (CommonEntityInfo. name fields-info)
+    (throw (IllegalArgumentException.))))
+
+(defn- check-fields-info
+  "Given fields-info list, checks that all fields (field? returns true) and
+  columns (column? returns true) have different field-order and column-order
+  respectively"
+  [fields-info]
+  (let [fields (filter #(.field? %) fields-info)
+        columns (filter #(.column? %) fields-info)
+        field-order-set (into #{} (map #(.field-order %) fields))
+        column-order-set (into #{} (map #(.column-order %) columns))]
+    (and (= (count fields) (count field-order-set))
+      (= (count columns) (count column-order-set)))))
+
+(defn- default-field-value
+  [field-info]
+  (hash-map (.sql-name field-info) (.default field-info)))
--- test/info_test.clj
+++ test/info_test.clj
@@ -3,42 +3,63 @@
   video_guide.info_test
   (:require [clojure.test :refer :all]
             [video_guide.ext :refer :all]
-            [video_guide.info])
+            [video_guide.info :refer [make-entity-info]])
   (:import (video_guide.info FieldInfo CommonEntityInfo)))
 
-(defrecord SQLFieldInfo [id]
+(defrecord SQLFieldInfo [name]
   FieldInfo
+  (field [_] name)
+  (label [_] name)
+  (getter [_] true)
+  (default [_] 0)
+  (column [_] name)
+  (sql-field [_] (list name :sql))
+  (sql-name [_] name)
   (field-order [_] 0)
   (column-order [_] 0)
   (field? [_] false)
   (column? [_] false)
   (sql? [_] true))
 
-(defrecord FormFieldInfo [id field-order]
+(defrecord FormFieldInfo [name field-order]
   FieldInfo
+  (field [_] name)
+  (label [_] name)
+  (getter [_] true)
+  (default [_] 0)
+  (column [_] name)
+  (sql-field [_] (list name :sql))
+  (sql-name [_] name)
   (field-order [_] field-order)
   (column-order [_] 0)
   (field? [_] true)
   (column? [_] false)
   (sql? [_] false))
 
-(defrecord ColumnFieldInfo [id column-order]
+(defrecord ColumnFieldInfo [name column-order]
   FieldInfo
+  (field [_] name)
+  (label [_] name)
+  (getter [_] true)
+  (default [_] 0)
+  (column [_] name)
+  (sql-field [_] (list name :sql))
+  (sql-name [_] name)
   (field-order [_] 0)
   (column-order [_] column-order)
   (field? [_] false)
   (column? [_] true)
   (sql? [_] false))
 
-(def sql-field1 (SQLFieldInfo. 1))
-(def sql-field2 (SQLFieldInfo. 2))
-(def sql-field3 (SQLFieldInfo. 3))
-(def form-field1 (FormFieldInfo. 4 1))
-(def form-field2 (FormFieldInfo. 5 2))
-(def form-field3 (FormFieldInfo. 6 3))
-(def column-field1 (ColumnFieldInfo. 7 1))
-(def column-field2 (ColumnFieldInfo. 8 2))
-(def column-field3 (ColumnFieldInfo. 9 3))
+(def sql-field1 (SQLFieldInfo. :name))
+(def sql-field2 (SQLFieldInfo. :duration))
+(def sql-field3 (SQLFieldInfo. :width))
+(def form-field1 (FormFieldInfo. :name 1))
+(def form-field2 (FormFieldInfo. :duration 2))
+(def form-field3 (FormFieldInfo. :width 3))
+(def column-field1 (ColumnFieldInfo. :name 1))
+(def column-field2 (ColumnFieldInfo. :duration 2))
+(def column-field3 (ColumnFieldInfo. :width 3))
 
 (deftest entity-info-sql-fields-test
   (let [entity-test1 (CommonEntityInfo. "test1" [])
@@ -46,7 +67,7 @@
         entity-test3 (CommonEntityInfo. "test3"
                        [sql-field1 form-field1 sql-field2 column-field1
                         sql-field3])
-        test3-answer [sql-field1 sql-field2 sql-field3]]
+        test3-answer (map #(.sql-field %) [sql-field1 sql-field2 sql-field3])]
     (is (= [] (.sql-fields entity-test1))
       "Should return empty vector for empty EntityInfo")
     (is (= [] (.sql-fields entity-test2))
@@ -98,5 +119,47 @@
       "Should return sorted by column-order vector of column-typed FieldInfo
       from EntityInfo")))
 
-(run-tests 'video_guide.info_test)
+(deftest make-entity-info-test
+  (let [fields-info1 [sql-field1 form-field1 column-field1]
+        fields-info2 [sql-field1 sql-field1 form-field3 column-field3
+                      form-field1 column-field2]
+        fields-info3 [form-field1 column-field3 form-field1 sql-field3
+                      form-field2]
+        fields-info4 [column-field3 form-field2 sql-field3 column-field1
+                      column-field1]]
+    (is (thrown? IllegalArgumentException
+          (make-entity-info "test1" []))
+      "Should throw IllegalArgumentException, when fields-info is empty")
+    (is (= (CommonEntityInfo. "test2" fields-info1)
+          (make-entity-info "test2" fields-info1))
+      "Should create CommonEntityInfo, when fields-info is correct")
+    (is (thrown? IllegalArgumentException
+          (make-entity-info "test3" [fields-info3]))
+      "Should throw IllegalArgumentException, when fields-info contains
+      different form-field-typed FieldInfo with same field-order")
+    (is (thrown? IllegalArgumentException
+          (make-entity-info "test4" [fields-info4]))
+      "Should throw IllegalArgumentException, when fields-info contains
+      different column-typed FieldInfo with same column-order")))
 
+(deftest default-entity-info-test
+  (let [entity-test1 (CommonEntityInfo. "test1" [])
+        entity-test2 (CommonEntityInfo. "test2" [sql-field1 column-field1])
+        entity-test3 (CommonEntityInfo. "test3"
+                       [form-field1 sql-field1 form-field2 column-field1
+                        form-field3])
+        test3-answer {:name 0 :duration 0 :width 0}]
+    (is (nil? (.default-entity entity-test1))
+      "Should return nil for empty EntityInfo")
+    (is (nil? (.default-entity entity-test2))
+      "Should return nil for EntityInfo without form-field-typed
+      FieldInfo")
+    (is (= test3-answer (.default-entity entity-test3))
+      "Should return map with form-field-typed FieldInfo keys from
+      EntityInfo and default values")))
+
+(deftest table-info-test
+  (is (= "test" (.table (CommonEntityInfo. "test" [])))
+    "Should return name of EntityInfo"))
+
+(run-tests 'video_guide.info_test)
